ff033e6c190079d17d59f5be82318141e24f1d1c
su-de-sh <sudesh7443@gmail.com>
2022-08-25 11:29:18 +0545

    feat: add creating note feature

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state);
+
+  const dispatch = useDispatch();
 
   const vote = (id) => {
     // console.log('vote', id)
-    dispatch({type:"VOTE",data:{id}})
-  }
+    dispatch({ type: "VOTE", data: { id } });
+  };
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    dispatch({ type: "ADD", data: { content } });
+    event.target.anecdote.value = "";
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
