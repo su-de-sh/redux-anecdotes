278931faa033a4f16a32572930f1c86c365c2d5c
su-de-sh <sudesh7443@gmail.com>
2022-08-26 12:21:47 +0545

    feat: fetch anecdotes from backend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { AnnecdoteList } from "./components/AnnecdoteList";
 import { AnecdoteForm } from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 import Filter from "./components/Filter";
+import anecdoteService from "./services/anecdoteService";
+import { appendNewAnecdote } from "./reducers/anecdoteReducer";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((response) => dispatch(appendNewAnecdote(response)));
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
